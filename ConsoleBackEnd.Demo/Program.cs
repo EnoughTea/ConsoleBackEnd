5046d6531a95ed96578a1b372b2cbd3f05b8c5ad
Konstantin Zakharov <enough.tea@outlook.com>
2020-07-04 19:31:36 +0300

    Actually use custom converter

--- ConsoleBackEnd.Demo/Program.cs
+++ ConsoleBackEnd.Demo/Program.cs
@@ -9,12 +9,12 @@ namespace ConsoleBackEnd.Demo
         {
             // Create instance containing several commands which call ExchangeRates API synchronously.
             var exchangeRatesApi = new ExchangeRatesApi();
-            
+
             // Slightly customize 'command result to string' converter,
             // since ExchangeRates returns ISO dates without time part.
             var returnConverter = new CommandReturnedObjectJsonConverter(true, Formatting.Indented,
                 new JsonSerializerSettings { DateFormatString = "yyyy'-'MM'-'dd" });
-            
+
             // Create command manager.
             var consoleCommands = new ConsoleCommands();
 
@@ -28,22 +28,22 @@ namespace ConsoleBackEnd.Demo
             });
             consoleCommands.Register(new ConsoleCommand(help.Target!, help.Method, "Help",
                 "Prints available commands.", new[] { "help", "commands" }));
-            
+
             var clearScreen = new Action(Console.Clear);
             consoleCommands.Register(new ConsoleCommand(clearScreen.Target!, clearScreen.Method, "ClearScreen",
                 "Clears console.", new[] { "cls" }));
-            
+
             var quit = new Action(() => Environment.Exit(0));
             consoleCommands.Register(new ConsoleCommand(quit.Target!, quit.Method, "Quit",
                 "Quits the program.", new[] { "q" }));
-            
+
             Console.WriteLine("Input \"help\" without quotes to see available commands.");
-            
+
             // Simple REPL:
             while (true) {
                 Console.Write("Command prompt: ");
                 string commandRepr = Console.ReadLine();
-                string evaluated = consoleCommands.Execute(commandRepr).ConvertOrError();
+                string evaluated = consoleCommands.Execute(commandRepr).ConvertOrError(returnConverter);
                 Console.WriteLine(evaluated);
             }
         }
